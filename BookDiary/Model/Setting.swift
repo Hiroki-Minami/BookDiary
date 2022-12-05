289ed987a51b0887716e2cda7031e08757cd034e
Quien <quien697@gmail.com>
2022-12-04 16:07:56 -0800

    chore: Add a struct of Setting in Model

---
+++ BookDiary/Model/Setting.swift
@@ -0,0 +1,14 @@
+//
+//  Setting.swift
+//  BookDiary
+//
+//  Created by Quien on 2022/12/4.
+//
+
+import Foundation
+
+
+/// <#Description#>
+struct Setting {
+  static var browse = Browses.Google
+}
