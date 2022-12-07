f727d253e6fd1ea8ccd7581b191621008db3f235
muramasa1113 <muramasa11139@icloud.com>
2022-12-07 15:16:12 -0800

    home

---
+++ BookDiary/HomeBookCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeBookCellTableViewCell.swift
+//  BookDiary
+//
+//  Created by 村上匡志 on 2022-12-06.
+//
+
+import UIKit
+
+class HomeBookCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
