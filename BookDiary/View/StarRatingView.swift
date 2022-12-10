85f3e4c19e4f0f4bcab064542b225e4021032204
Quien <quien697@gmail.com>
2022-12-10 13:35:32 -0800

    feat: Add a new view to show 5 star rating

---
+++ BookDiary/View/StarRatingView.swift
@@ -0,0 +1,20 @@
+//
+//  StarRatingView.swift
+//  BookDiary
+//
+//  Created by Quien on 2022/12/9.
+//
+
+import UIKit
+
+class StarRatingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
