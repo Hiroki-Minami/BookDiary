60c1874c66cdbb63d7e05483a120bef3fd431dc4
Hiroki Minami <hirokimnm6@gmail.com>
2022-12-12 16:51:36 -0800

    passing post data from home to each book

--- BookDiary/HomeTableViewController.swift
+++ BookDiary/HomeTableViewController.swift
@@ -8,14 +8,13 @@
 import UIKit
 
 protocol HomeTableViewControllerDelegate {
-  func titleButtonTapped(_ sender: Any) 
+  func titleButtonTapped(_ sender: Any)
 }
 
 class HomeTableViewController: UITableViewController, HomeTableViewControllerDelegate {
   
   var bookCell = [Post]()
   var tappedPost: Post?
-  var tappedCellIndexPath: IndexPath?
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -27,13 +26,13 @@ class HomeTableViewController: UITableViewController, HomeTableViewControllerDel
     }
     
   }
-
+  
   // The number of cells that are displayed at home screen
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return bookCell.count
   }
-
-  // path the date 
+  
+  // path the date
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCellIdentifier", for: indexPath) as! HomeBookCellTableViewCell
     let book = bookCell[indexPath.row]
@@ -45,49 +44,44 @@ class HomeTableViewController: UITableViewController, HomeTableViewControllerDel
     return cell
   }
   
-  
-  @IBSegueAction func eachBookDetail(_ coder: NSCoder, sender: Any?) -> EachBookViewController? {
-    let detailcontroller = EachBookViewController(coder: coder)
-    detailcontroller!.eachBook = tappedPost
-    // guard let cell = sender as? HomeBookCellTableViewCell, let indexPath = tableView.indexPath(for: cell) else {
-    //   return detailcontroller
-    // }
-    tableView.deselectRow(at: tappedCellIndexPath!, animated: true)
-    detailcontroller?.eachBook = tappedPost
-    // detailcontroller?.eachBook = bookCell[indexPath.row]
-    return detailcontroller
-  }
-
   func titleButtonTapped(_ sender: Any) {
     guard let cell = sender as? HomeBookCellTableViewCell, let indexPath = tableView.indexPath(for: cell) else { return }
     tappedPost = cell.post
-    tappedCellIndexPath = indexPath
+    
+    tableView.deselectRow(at: indexPath, animated: true)
+    performSegue(withIdentifier: "toEachBook", sender: self)
   }
   
-//  func titleButtonTapped(sender: HomeBookCellTableViewCell) {
-//    print(#function)
-//    if let indexPath = tableView.indexPath(for: sender) {
-//      var book = bookCell[indexPath.row]
-//      bookCell[indexPath.row] = book
-//      tableView.reloadRows(at: [indexPath], with: .automatic)
-//    }
-//  }
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if (segue.identifier == "toEachBook") {
+      let eachBookView = segue.destination as! EachBookViewController
+      eachBookView.eachBook = tappedPost
+    }
+  }
+  //  func titleButtonTapped(sender: HomeBookCellTableViewCell) {
+  //    print(#function)
+  //    if let indexPath = tableView.indexPath(for: sender) {
+  //      var book = bookCell[indexPath.row]
+  //      bookCell[indexPath.row] = book
+  //      tableView.reloadRows(at: [indexPath], with: .automatic)
+  //    }
+  //  }
   
-//  @IBAction func toEachBook(segue: UIStoryboardSegue) {
-//    guard segue.identifier == "toEachBook" else {return}
-//    let souceViewCntroller = segue.source as! EachBookViewController
-//  }
-//
-//  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//    if segue.identifier == "toEachBook" {
-//      if let indexPath = tableView.indexPathForSelectedRow {
-//        guard let destination = segue.destination as? EachBookViewController else {return}
-//        destination.eachBook = bookCell[indexPath.row]
-//      }
-//
-//    }
-//  }
-//
+  //  @IBAction func toEachBook(segue: UIStoryboardSegue) {
+  //    guard segue.identifier == "toEachBook" else {return}
+  //    let souceViewCntroller = segue.source as! EachBookViewController
+  //  }
+  //
+  //  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+  //    if segue.identifier == "toEachBook" {
+  //      if let indexPath = tableView.indexPathForSelectedRow {
+  //        guard let destination = segue.destination as? EachBookViewController else {return}
+  //        destination.eachBook = bookCell[indexPath.row]
+  //      }
+  //
+  //    }
+  //  }
+  //
 }
 
 
