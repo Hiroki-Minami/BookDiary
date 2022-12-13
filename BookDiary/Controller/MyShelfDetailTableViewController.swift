5e9a13a44946d44d47e60bda3944ab9ca36a36ef
Quien <quien697@gmail.com>
2022-12-12 17:49:14 -0800

    chore: 1. removed property 'language' from strtuct 'post', and moved everthing about language.  2. changed property 'genres' to be no optional from struct 'post'.

--- BookDiary/Controller/MyShelfDetailTableViewController.swift
+++ BookDiary/Controller/MyShelfDetailTableViewController.swift
@@ -7,14 +7,12 @@
 
 import UIKit
 
-class MyShelfDetailTableViewController: UITableViewController, RatingViewDelegate, LanguageListDelegate, GenreListDelegate {
+class MyShelfDetailTableViewController: UITableViewController, RatingViewDelegate, GenreListDelegate {
   
   @IBOutlet weak var titleTextField: UITextField!
   @IBOutlet weak var AutherTextField: UITextField!
   @IBOutlet weak var ratesView: StarRatingView!
   @IBOutlet weak var reviewTextView: UITextView!
-  @IBOutlet weak var languageLabel: UILabel!
-  @IBOutlet weak var languageCountLabel: UILabel!
   @IBOutlet weak var genreLabel: UILabel!
   @IBOutlet weak var genreCountLabel: UILabel!
   @IBOutlet weak var saveButton: UIBarButtonItem!
@@ -29,12 +27,6 @@ class MyShelfDetailTableViewController: UITableViewController, RatingViewDelegat
     ratesView.delegate = self
     updateView()
     updateSaveButtonState()
-  }
-  
-  @IBSegueAction func selectLanguage(_ coder: NSCoder, sender: Any?) -> LanguageListTableViewController? {
-    let detailController = LanguageListTableViewController(coder: coder)
-    detailController?.language = Language(rawValue: languageLabel.text!)
-    return detailController
   }
   
   @IBSegueAction func selectGenre(_ coder: NSCoder, sender: Any?) -> GenreListTableViewController? {
@@ -58,12 +50,9 @@ class MyShelfDetailTableViewController: UITableViewController, RatingViewDelegat
       AutherTextField.text = post.author
       ratesView.ratingValue = post.rates!
       reviewTextView.text = post.review!
-      languageLabel.text = post.language.rawValue
-      genreLabel.text = post.genres!.rawValue
-      language = post.language
+      genreLabel.text = post.genres.rawValue
       genre = post.genres
     }
-    languageCountLabel.text = String(Language.allCases.count)
     genreCountLabel.text = String(Genres.allCases.count)
   }
   
@@ -72,22 +61,15 @@ class MyShelfDetailTableViewController: UITableViewController, RatingViewDelegat
     let shouldEnableSaveButton =
       (titleTextField.text?.isEmpty == false) &&
       (AutherTextField.text?.isEmpty == false) &&
-      (languageLabel.text?.isEmpty == false) &&
-      !notYet.contains(languageLabel.text!) &&
       (genreLabel.text?.isEmpty == false) &&
       !notYet.contains(genreLabel.text!)
     saveButton.isEnabled = shouldEnableSaveButton
   }
   
   // MARK: - Delegate
+  
   func updateRatingFormatValue(_ value: Float) {
     rates = value
-  }
-  
-  func didSelect(language: Language) {
-    self.language = language
-    languageLabel.text = language.rawValue
-    updateSaveButtonState()
   }
   
   func didSelect(genre: Genres) {
@@ -97,6 +79,7 @@ class MyShelfDetailTableViewController: UITableViewController, RatingViewDelegat
   }
   
   // MARK: -
+  
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if segue.identifier == "saveUnwind" {
       let title = titleTextField.text!
@@ -108,15 +91,10 @@ class MyShelfDetailTableViewController: UITableViewController, RatingViewDelegat
         post?.author = auther
         post?.rates = rates
         post?.review = review
-        post?.language = language!
         post?.genres = genre!
       } else {
-        post = Post(title: title, author: auther, rates: rates, language: Language.English, genres: Genres(rawValue: genre!.rawValue), review: review, postedDate: Date(), poster: Poster(firstName: "Quien", nickName: "Quien"))
+        post = Post(title: title, author: auther, rates: rates, genres: Genres(rawValue: genre!.rawValue)!, review: review, postedDate: Date(), poster: Poster(firstName: "Quien", nickName: "Quien"))
       }
-    } else if segue.identifier == "selectLanguage" {
-        let destinationViewController = segue.destination as? LanguageListTableViewController
-        destinationViewController?.delegate = self
-        destinationViewController?.language = language
     } else if segue.identifier == "selectGenre" {
       let destinationViewController = segue.destination as? GenreListTableViewController
       destinationViewController?.delegate = self
--- BookDiary/Model/Post.swift
+++ BookDiary/Model/Post.swift
@@ -14,22 +14,20 @@ struct Post: Equatable, Codable {
   var isComplete: Bool
   var author: String
   var rates: Float?
-  var language: Language
-  var genres: Genres?
+  var genres: Genres
   var review: String?
   var postedDate: Date
   var poster: Poster
   
   private static let archiveURL = FileManager.pathToDoucumentsDirectory(with: "posts")
   
-  init(title: String, img: String? = nil, isComplete: Bool = false, author: String, rates: Float? = nil, language: Language, genres: Genres? = nil, review: String? = nil, postedDate: Date, poster: Poster) {
+  init(title: String, img: String? = nil, isComplete: Bool = false, author: String, rates: Float? = 0.0, genres: Genres, review: String? = nil, postedDate: Date, poster: Poster) {
     self.id = UUID()
     self.title = title
     self.img = img
     self.isComplete = isComplete
     self.author = author
     self.rates = rates
-    self.language = language
     self.genres = genres
     self.review = review
     self.postedDate = postedDate
@@ -64,16 +62,16 @@ struct Post: Equatable, Codable {
     let poster3 = Poster(firstName: "poster3", nickName: "Henry")
     let poster4 = Poster(firstName: "poster4")
     
-    let post1 = Post(title: "book1", author: "author1", language: Language.English, genres: Genres.Fantasy, review: "It's an amazing book.", postedDate: Date(), poster: poster1)
-    let post2 = Post(title: "book2", author: "author2", language: Language.Mandarin, genres: Genres.Art, review: "It's an amazing book.", postedDate: Date(), poster: poster1)
-    let post3 = Post(title: "book3", author: "author3", language: Language.English, genres: Genres.Business, review: "It's an amazing book.", postedDate: Date(), poster: poster2)
-    let post4 = Post(title: "book4", author: "author4", language: Language.English, genres: Genres.Fantasy, review: "It's an amazing book.", postedDate: Date(), poster: poster2)
-    let post5 = Post(title: "book5", author: "author5", language: Language.Japanese, genres: Genres.Science, review: "It's an amazing book.", postedDate: Date(), poster: poster2)
-    let post6 = Post(title: "book6", author: "author6", language: Language.English, genres: Genres.Fantasy, review: "It's an amazing book.", postedDate: Date(), poster: poster2)
-    let post7 = Post(title: "book7", author: "author7", language: Language.English, genres: Genres.Art, review: "It's an amazing book.", postedDate: Date(), poster: poster3)
-    let post8 = Post(title: "book8", author: "author8", language: Language.Korean, genres: Genres.Fantasy, review: "It's an amazing book.", postedDate: Date(), poster: poster4)
-    let post9 = Post(title: "book9", author: "author9", language: Language.English, genres: Genres.Business, review: "It's an amazing book.", postedDate: Date(), poster: poster4)
-    let post10 = Post(title: "book10", author: "author10", language: Language.English, genres: Genres.Fantasy, review: "It's an amazing book.", postedDate: Date(), poster: poster1)
+    let post1 = Post(title: "book1", author: "author1", genres: Genres.Fantasy, review: "It's an amazing book.", postedDate: Date(), poster: poster1)
+    let post2 = Post(title: "book2", author: "author2", genres: Genres.Art, review: "It's an amazing book.", postedDate: Date(), poster: poster1)
+    let post3 = Post(title: "book3", author: "author3", genres: Genres.Business, review: "It's an amazing book.", postedDate: Date(), poster: poster2)
+    let post4 = Post(title: "book4", author: "author4", genres: Genres.Fantasy, review: "It's an amazing book.", postedDate: Date(), poster: poster2)
+    let post5 = Post(title: "book5", author: "author5", genres: Genres.Science, review: "It's an amazing book.", postedDate: Date(), poster: poster2)
+    let post6 = Post(title: "book6", author: "author6", genres: Genres.Fantasy, review: "It's an amazing book.", postedDate: Date(), poster: poster2)
+    let post7 = Post(title: "book7", author: "author7", genres: Genres.Art, review: "It's an amazing book.", postedDate: Date(), poster: poster3)
+    let post8 = Post(title: "book8", author: "author8", genres: Genres.Fantasy, review: "It's an amazing book.", postedDate: Date(), poster: poster4)
+    let post9 = Post(title: "book9", author: "author9", genres: Genres.Business, review: "It's an amazing book.", postedDate: Date(), poster: poster4)
+    let post10 = Post(title: "book10", author: "author10", genres: Genres.Fantasy, review: "It's an amazing book.", postedDate: Date(), poster: poster1)
     
     return [post1, post2, post3, post4, post5, post6, post7, post8, post9, post10]
   }
